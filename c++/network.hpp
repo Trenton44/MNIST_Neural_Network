5e20f73e16bbbabea66b8632de58296bd2d675b2
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2023-02-24 20:18:06 -0600

    Updated network.hpp with new functions

--- c++/network.hpp
+++ c++/network.hpp
@@ -8,15 +8,18 @@ typedef std::vector<double> Sample;
 class Network {
     public:
         Network(const std::vector<unsigned> &topology);
-        void results(std::vector<double> &result_values);
-        void train(const std::vector<Sample> &data, const std::vector<double> target_values, const unsigned epochs);
+        unsigned results(void);
+        double train(std::string network_save_filename, const std::vector<Sample> &data, const std::vector<double> target_values, const unsigned epochs, const unsigned sample_count);
+        void predict(const std::vector<double> &data);
+        bool save(std::string filename);
+        static Network load(std::string filename);
         void print();
+
     private:
         std::vector<Layer> layers;
         double error;
         void forwards(const std::vector<double> &data);
-        void backwards(const std::vector<double> &target_values);
+        double backwards(const std::vector<double> &target_values);
 };
-
 
 #endif
